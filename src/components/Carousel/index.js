33529469c5b0701ce1861773b8fa10eef7ef237b
meliodas113 <anupam.fas27@gmail.com>
2023-12-27 02:02:09 +0530

    carousel added

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect,useRef,useCallback } from 'react';
+import './styles.css';
+import axios from 'axios';
+
+import { Box } from '@mui/material';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import SongCard from '../SongCard';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+function Carousel({url}) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const sliderRef = useRef(null);
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://qtify-backend-labs.crio.do/albums/${url}`
+        );
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <Box style={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+        <Box className="nav-btn" onClick={handlePrev}>
+          <img src='/assets/arrow-l.png' width={32} height={32} alt='Qtify' />
+        </Box>
+        <Swiper
+          ref={sliderRef}
+          slidesPerView={7}
+          spaceBetween={10}
+          loop={true}
+          className='mySwiper'
+        >
+          {data &&
+            data.map((item) => {
+              return (
+                <SwiperSlide>
+                  <Box key={item.id} className='Section-Card'>
+                    <SongCard
+                      image={item.image}
+                      follows={item.follows}
+                      title={item.title}
+                    />
+                  </Box>{' '}
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
+        <Box className="nav-btn" onClick={handleNext}>
+          <img src='/assets/arrow-r.png' width={32} height={32} alt='Qtify' />
+        </Box>{' '}
+      </Box>
+    </>
+  );
+}
+
+export default Carousel;
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -4,9 +4,11 @@ import axios from 'axios';
 import './styles.css';
 
 import { Box, Grid, Item } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
 import SongCard from '../SongCard';
+import Carousel from '../Carousel';
 
-function Section({heading,api}) {
+function Section({ heading, api }) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [expand, setExpand] = useState(false);
@@ -30,23 +32,38 @@ function Section({heading,api}) {
     <Box className='Section'>
       <Box className='Section-Heading'>
         <p>{heading}</p>
-        <p onClick={()=>setExpand((exp) => !exp)} className='Section-SubHeading'>
-            {expand ? "Collapse" : "Show all"}
+        <p
+          onClick={() => setExpand((exp) => !exp)}
+          className='Section-SubHeading'
+        >
+          {expand ? 'Collapse' : 'Show all'}
         </p>
       </Box>
-      <Box className='Section-Grid' sx={{ width: '90%', height: (expand ? "auto" : "270px") }}>
-        {data &&
+      <Box
+        className='Section-Grid'
+        sx={{ width: '90%', height: expand ? 'auto' : '270px' }}
+      >
+        {expand ? (
+          data &&
           data.map((item) => {
             return (
-              <Box key={item.id} className='Section-Card'>
-                <SongCard
-                  image={item.image}
-                  follows={item.follows}
-                  title={item.title}
-                />
-              </Box>
+              <Tooltip
+                title={`${item.songs.length} Song`}
+                placement='top-start'
+              >
+                <Box key={item.id} className='Section-Card'>
+                  <SongCard
+                    image={item.image}
+                    follows={item.follows}
+                    title={item.title}
+                  />
+                </Box>{' '}
+              </Tooltip>
             );
-          })}
+          })
+        ) : (
+          <Carousel url={api}/>
+        )}
       </Box>
     </Box>
   );
